0ad442771463febff95e7a32ad42000d4b9a1443
Christine Zheng <zchristine11@gmail.com>
2023-03-20 15:07:40 -0400

    update Products page with filter/sort & <List>, and design

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import List from '../../components/List/List';
+import './Products.scss';
 
 const Products = () => {
-  return <div>Products</div>;
+  const param = useParams();
+  const catId = parseInt(param.id);
+
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
+
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Skirts</label>
+          </div>
+
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3} />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+
+        <div className="filterItem">
+          <h2>Filter by Price</h2>
+          <div className="inputItem">
+            <div className="filterPrice">
+              <span className="spanMin">$0</span>
+              <input
+                type="range"
+                min={0}
+                max={1000}
+                onChange={(e) => setMaxPrice(e.target.value)}
+              />
+              <span className="spanMax">${maxPrice}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="filterItem">
+          <h2>Sort By</h2>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort('asc')}
+            />
+            <label htmlFor="asc">Price Low to High</label>
+          </div>
+
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort('desc')}
+            />
+            <label htmlFor="desc">Price High to Low</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+
+        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
