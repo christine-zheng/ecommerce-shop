533432a7f34d2548725cf8a255a953a3e2e95758
Christine Zheng <zchristine11@gmail.com>
2023-03-28 00:39:36 -0400

    fetch category's subcats & add selectedSubCats to the state

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import List from '../../components/List/List';
+import useFetch from '../../hooks/useFetch';
 import './Products.scss';
 
+// display the products for the selected category (ie women, men, sale)
 const Products = () => {
   const param = useParams();
   const catId = parseInt(param.id);
 
+  // state
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState(null);
+  const [selectedSubCats, setSelectedSubCats] = useState([]);
+
+  // fetch all subcategories (ie t-shirt, dress) with the related category ID
+  const { data, loading, error } = useFetch(
+    `/sub-categories?filters[categories][id][$eq]=${catId}`
+  );
+
+  // update the sub-categories based on user selection
+  const handleSubCats = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter((item) => item !== value)
+    );
+  };
 
   return (
     <div className="products">
@@ -16,20 +37,17 @@ const Products = () => {
         <div className="filterItem">
           <h2>Product Categories</h2>
 
-          <div className="inputItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Shoes</label>
-          </div>
-
-          <div className="inputItem">
-            <input type="checkbox" id="2" value={2} />
-            <label htmlFor="2">Skirts</label>
-          </div>
-
-          <div className="inputItem">
-            <input type="checkbox" id="3" value={3} />
-            <label htmlFor="3">Coats</label>
-          </div>
+          {data?.map((item) => (
+            <div className="inputItem" key={item.id}>
+              <input
+                type="checkbox"
+                id={item.id}
+                value={item.id}
+                onChange={handleSubCats}
+              />
+              <label htmlFor={item.id}>{item.attributes.title}</label>
+            </div>
+          ))}
         </div>
 
         <div className="filterItem">
@@ -81,7 +99,12 @@ const Products = () => {
           alt=""
         />
 
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <List
+          catId={catId}
+          maxPrice={maxPrice}
+          sort={sort}
+          subCats={selectedSubCats}
+        />
       </div>
     </div>
   );
