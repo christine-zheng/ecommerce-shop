f172daae1593c9163bb6a02732138fd40813e636
Christine Zheng <zchristine11@gmail.com>
2023-03-28 01:13:13 -0400

    update List to fetch data using the filters from Products page

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,75 +1,28 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 import Card from '../Card/Card';
 import './List.scss';
 
-// component for fetching [filtered] data
-// if data exists, map each item to the Card component
-const List = () => {
-  const data = [
-    {
-      id: 1,
-      img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      img2: 'https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Long Sleeve Graphic T-Shirt',
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Beanie',
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: 'https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Coat',
-      isNew: false,
-      oldPrice: 215,
-      price: 165,
-    },
-    {
-      id: 4,
-      img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Blouse',
-      isNew: false,
-      oldPrice: 63,
-      price: 45,
-    },
-    {
-      id: 5,
-      img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Blouse',
-      isNew: false,
-      oldPrice: 63,
-      price: 45,
-    },
-    {
-      id: 6,
-      img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Blouse',
-      isNew: false,
-      oldPrice: 63,
-      price: 45,
-    },
-    {
-      id: 7,
-      img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      title: 'Blouse',
-      isNew: false,
-      oldPrice: 63,
-      price: 45,
-    },
-  ];
+// Display [filtered] products
+const List = ({ catId, maxPrice, sort, subCats }) => {
+  // fetch data:
+  // filter by category ID
+  // then filter by the selected sub-categories
+  // sort by asc or desc
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&filters[categories][id][$eq]=${catId}${subCats.map(
+      (item) => `&filters[sub_categories][id][$eq]=${item}`
+    )}&filters[price][$lte]=${maxPrice}${sort ? `&sort=price:${sort}` : ''}`
+  );
 
+  // if data exists, map each item to the Card component
   return (
     <div className="list">
-      {data?.map((item) => (
-        <Card key={item.id} item={item} />
-      ))}
+      {error
+        ? 'Something went wrong!'
+        : loading
+        ? 'loading...'
+        : data?.map((item) => <Card key={item.id} item={item} />)}
     </div>
   );
 };
