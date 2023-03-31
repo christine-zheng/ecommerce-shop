2c522f38b0bbc32fc538acd47d5049f206258c7c
Christine Zheng <zchristine11@gmail.com>
2023-03-31 18:26:27 -0400

    update <Footer> & add design for layout-breakpoint-small

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,59 +1,59 @@
 import React from 'react';
 import './Footer.scss';
 
+// Material UI icons
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 const Footer = () => {
   return (
     <div className="footer">
       <div className="top">
         <div className="item">
-          <h1>Categories</h1>
-          <span>Women</span>
-          <span>Men</span>
-          <span>Shoes</span>
-          <span>Accessories</span>
-          <span>New Arrivals</span>
+          <h1>Account</h1>
+          <span>Register</span>
+          <span>Order Status</span>
+          <span>Membership Program</span>
         </div>
 
         <div className="item">
-          <h1>Links</h1>
-          <span>FAQ</span>
-          <span>Pages</span>
-          <span>Stores</span>
-          <span>Compare</span>
-          <span>Cookies</span>
+          <h1>Support</h1>
+          <span>Help Center</span>
+          <span>Return Policy</span>
+          <span>Shipping Info</span>
         </div>
 
         <div className="item">
           <h1>About</h1>
-          <span>
-            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
-            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
-            ut labore etdolore.
-          </span>
+          <span>Our Story</span>
+          <span>Stores</span>
+          <span>Careers</span>
         </div>
 
         <div className="item">
           <h1>Contact</h1>
+          <span>Live Chat</span>
+          <span>Email Sign Up</span>
           <span>
-            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
-            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
-            ut labore etdolore.
+            <div className="icons">
+              <FacebookIcon />
+              <InstagramIcon />
+              <TwitterIcon />
+            </div>
           </span>
         </div>
       </div>
 
       <div className="bottom">
         <div className="left">
-          <div className="logo">Lamastore</div>
-          <span className="copyright">
-            © Copyright 2023. All Rights Reserved
-          </span>
+          <img src="/img/payment.png" alt="Payment methods" />
         </div>
 
         <div className="right">
-          <img src="/img/payment.png" alt="Payment methods" />
+          <span className="copyright">
+            © 2023 Urban Apparel All Rights Reserved
+          </span>
         </div>
       </div>
     </div>
