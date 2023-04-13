b72748332614106104546a47d2e85544dc72c82e
Christine Zheng <zchristine11@gmail.com>
2023-04-12 23:13:10 -0400

    create Accordion component and style

---
+++ client/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './Accordion.scss';
+
+// Material UI icon
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Accordion = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const newContent = content.split('\n');
+  console.log('new content: ', newContent);
+
+  return (
+    <div className="accordion">
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <div>
+          {isActive ? (
+            <RemoveIcon className="icon" />
+          ) : (
+            <AddIcon className="icon" />
+          )}
+        </div>
+      </div>
+      {isActive && (
+        <div className="accordion-content">
+          {newContent.map((line) => (
+            <p className="accordion-p">{line}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Accordion;
